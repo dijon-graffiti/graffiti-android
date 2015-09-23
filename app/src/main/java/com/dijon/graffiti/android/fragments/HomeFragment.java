bdaec1c9f865650c9b6cbb4f17f30a8d2a3f50f7
John Shelley <john.shelley.p@gmail.com>
2015-09-22 22:38:40 -0500

    Init camera view. WIP needs some love

--- app/src/main/java/com/dijon/graffiti/android/fragments/HomeFragment.java
+++ app/src/main/java/com/dijon/graffiti/android/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.dijon.graffiti.android.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,11 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.dijon.graffiti.R;
+import com.dijon.graffiti.android.activities.CameraActivity;
+import com.dijon.graffiti.android.views.AutoFitTextureView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     public HomeFragment() {
     }
@@ -20,5 +23,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        view.findViewById(R.id.button_camera).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_camera: {
+                startActivity(new Intent(getContext(), CameraActivity.class));
+                break;
+            }
+        }
     }
 }
